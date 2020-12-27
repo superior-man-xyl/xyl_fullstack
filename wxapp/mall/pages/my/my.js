890ac131b862b197090e5bb9d2b6ffb4003b1d71
superior-man-xyl <15170725069@163.com>
2020-12-27 16:36:59 +0800

    vant和小程序学习

---
+++ wxapp/mall/pages/my/my.js
@@ -0,0 +1,74 @@
+/*
+ * @Author: xyl
+ * @Date: 2020-12-27 14:25:47
+ * @LastEditTime: 2020-12-27 15:00:43
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \git_pro\xyl_fullstack\wxapp\mall\pages\my\my.js
+ */
+// pages/my/my.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    avatar:'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fa0.att.hudong.com%2F52%2F62%2F31300542679117141195629117826.jpg&refer=http%3A%2F%2Fa0.att.hudong.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1611644385&t=14802e5f9567cae84a828b3451277f41'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ wxapp/mall/pages/my/readme.md
@@ -0,0 +1,21 @@
+<!--
+ * @Author: xyl
+ * @Date: 2020-12-27 14:33:01
+ * @LastEditTime: 2020-12-27 16:13:21
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \git_pro\xyl_fullstack\wxapp\mall\pages\my\readme.md
+-->
+研究页面怎么先出现页面,再加载出一些难加载的图片
+图片懒加载
+
+如何学习vant?
+- 看文档
+- 看官方demo
+
+user-poster:头图,
+图片的比例一定要对,不然就会失真
+542/288=100vw/53.14vw
+ <!-- width: 100%;
+    height: 53vw; -->
+    
