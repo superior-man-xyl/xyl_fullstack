bd067536cd2d7047bb3f56aca2ed42028cc4a73d
superior-man-xyl <15170725069@163.com>
2021-03-02 22:42:59 +0800

    hello world

--- react/react16_learn/demo_01/src/App.js
+++ react/react16_learn/demo_01/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{Component} from 'react'
+//上面是使用了es6的特性，解构写法，相当于下面
+// import React from 'react';
+// const Component=React.Component;
+class App extends Component{//App继承自Component
+    render(){
+        return(
+            <h1>Hello World</h1>
+        )
+    }
 }
-
-export default App;
+export default App;//为了能引用模块，要暴露出去
