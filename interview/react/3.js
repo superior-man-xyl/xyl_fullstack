8d4eea224ec67492432b491a39238dafacff848f
superior-man-xyl <15170725069@163.com>
2021-03-24 19:27:36 +0800

    纯函数与immutable与react

--- interview/react/3.js
+++ interview/react/3.js
@@ -8,17 +8,15 @@ let a=Map({
 //引用类似于 a,b
 let b=a.set('select','people');
 console.log(a==b);//false
-
 console.log(a.get('filter')===b.get('filter'));//true
-//为什么会是true   看看immutable
+//为什么会是true   看看immutable  个别改变(需要改变的改变)，其余还是不变，所以filter的对象还是同一个
 
 const obj={
     count:0
 }
 
 const immutableState=fromJS(obj);
-console.log(immutableState.get('count'));
+console.log(immutableState.get('count'));//0
 const jsObj=immutableState.toJS();
-jsObj.count=2;
 
-console.log(obj);
+console.log(obj);//{ count: 0 }  说明深拷贝成功了
