2d8c5460de00a93e34ee2b964b77151ed32f822a
superior-man-xyl <15170725069@163.com>
2021-04-26 19:02:09 +0800

    为扁平化增加一种方法

---
+++ interview/手写题/数组扁平化/4.js
@@ -0,0 +1,7 @@
+function flatten(arr){
+    return arr.toString().split(',');
+}
+console.log(flatten([[1,[1],[[2]]]]));//输出[ '1', '1', '2' ]
+
+console.log([[1,[1],[[2]]]].toString());//输出字符串 '1,1,2'
+console.log([[1,[1],[[2]]]].toString().split(','));//输出数组 [ '1', '1', '2' ]
