dc66b39b2f1d0724106ad10cfc99c86cfab18464
superior-man-xyl <15170725069@163.com>
2021-05-21 19:40:52 +0800

    手写instanceof

---
+++ interview/手写题/1.js
@@ -0,0 +1,15 @@
+// var fun=()=>{
+//     this.a=10;
+// }
+// var func=new fun();
+// console.log(func);
+let a = 'Hello World!';
+function first() {
+  console.log('Inside first function');
+  second();
+  console.log('Again inside first function');
+}
+function second() {
+  console.log('Inside second function');
+}
+first()
---
+++ interview/手写题/instanceof/1.js
@@ -0,0 +1,15 @@
+//instance使用：arr instanceof Array;
+function myInstanceof(left,right){
+    let proto=Object.getPrototypeOf(left);//获取到left的原型
+    let prototype=right.prototype;//获取到构造函数的原型对象
+    while(true){//判断构造函数的原型是否在对象的原型链上
+        if(!proto){
+            return false;
+        }
+        if(proto==prototype){
+            return true;
+        }
+        proto=Object.getPrototypeOf(proto);//原型链继续向下走
+    }
+
+}
